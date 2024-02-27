7a1fdd1dd6c605eb0f044a736ff8cc1415f1ef25
Debbie Yuen <deborahyuen2016@berkeley.edu>
2024-02-26 17:15:47 -0800

    visionOS app

---
+++ MyFirstVisionOS/MyFirstVisionOS/MyFirstVisionOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstVisionOSApp.swift
+//  MyFirstVisionOS
+//
+//  Created by Debbie Yuen on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstVisionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
